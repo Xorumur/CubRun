d23268c5b9a7a380d42cb68a8dc7ae04687f252e
mlecherb <math.lecherbonnier@gmail.com>
2026-04-03 12:48:58 +0200

    feat: import lib and set up architecture of the project

---
+++ include/cubrun.h
@@ -0,0 +1,7 @@
+#ifndef CUBRUN_H
+#define CUBRUN_H
+
+#include "../lib/libft/libft.h"
+#include "../lib/ft_printf/ft_printf.h"
+
+#endif
---
+++ src/main.c
@@ -0,0 +1,7 @@
+#include "../include/cubrun.h"
+
+
+int main(int ac, char **av) {
+    (void)ac;
+    ft_printf("%s\n", av[0]);
+}
